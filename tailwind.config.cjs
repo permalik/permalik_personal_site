5578c40f7f63cf7f22a32dc1e08d40f8bd954c99
permalik <permalik@protonmail.com>
2023-07-07 22:24:01 -0500

    feat: set main section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'green900': '#04110b'
+		},
 		extend: {
       fontFamily: {
         'sans': ['Fira Sans', ...defaultTheme.fontFamily.sans],
